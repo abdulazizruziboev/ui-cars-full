47b5e2550762199e935a9e8d5e7e53195b32c42a
abdulaziz <telegrampreimum4@gmail.com>
2026-01-08 14:30:57 +0000

    first commit

---
+++ js/dom_elements.mjs
@@ -0,0 +1,23 @@
+const 
+el_theme_changer = document.querySelector(".js-theme-changer"),
+el_moon = document.querySelector(".js-moon"),
+el_sun = document.querySelector(".js-sun"),
+el_skeleton_templates = document.querySelector(".js-skeleton-template"),
+el_cards_templates = document.querySelector(".js-cards-template"),
+el_skeleton_box = document.querySelector(".js-skeleton-box"),
+el_cards_box = document.querySelector(".js-cards-box"),
+el_pagination_box = document.querySelector(".js-pagination-box"),
+el_pagination_previous = document.querySelector(".js-pagination-previous"),
+el_pagination_next = document.querySelector(".js-pagination-next");
+export {
+  el_theme_changer,
+  el_moon,
+  el_sun,
+  el_skeleton_templates,
+  el_cards_templates,
+  el_skeleton_box,
+  el_cards_box,
+  el_pagination_box,
+  el_pagination_previous,
+  el_pagination_next
+};
---
+++ js/main_app.mjs
@@ -0,0 +1,14 @@
+import {
+  el_theme_changer,
+  el_moon,
+  el_sun,
+  el_skeleton_templates,
+  el_cards_templates,
+  el_skeleton_box,
+  el_cards_box,
+  el_pagination_box,
+  el_pagination_previous,
+  el_pagination_next
+} from "./dom_elements.mjs";
+
+console.log(el_skeleton_box);
