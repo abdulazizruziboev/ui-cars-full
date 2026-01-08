ee63442aa560afa931d5bbc8e65d301c4cb88383
abdulaziz <telegrampreimum4@gmail.com>
2026-01-08 15:39:10 +0000

    Initial commit

--- js/main_app.mjs
+++ js/main_app.mjs
@@ -10,5 +10,49 @@ import {
   el_pagination_previous,
   el_pagination_next
 } from "./dom_elements.mjs";
+let api_skip = 0,
+api_limit = 2;
+mainRequests();
+function mainRequests() {
+    skeletonUI(true,api_limit);
+    fetch(`https://json-api.uz/api/project/fn44-amaliyot/cars?skip=${api_skip}&limit=${api_limit}`)
+    .then((res)=>res.json())
+    .then((res)=>{
+        skeletonUI(false);
+        cardsUI(res.data)
+    })
+}
+function skeletonUI(bool,el_limit) {
+el_skeleton_box.innerHTM=null;
+el_cards_box.innerHTML=null;
+if(bool==true) {
+Array.from({length: el_limit}).forEach(()=>{
+    let el_clone_skeleton=el_skeleton_templates.cloneNode(true).content;
+    el_skeleton_box.append(el_clone_skeleton);
+});} else if(bool==false) {
+    el_skeleton_box.innerHTML=null;
+};};
 
-console.log(el_skeleton_box);
+function cardsUI(data) {
+    el_pagination_box.style.display="flex"
+    data.forEach((el,inx)=>{
+        let el_clone_card = el_cards_templates.cloneNode(true).content;
+        el_clone_card.querySelector(".js-car-title").textContent=el.name?el.name:"No data";
+        el_clone_card.querySelector(".js-car-trim").textContent=el.trim?el.trim:"No data";
+        el_clone_card.querySelector(".js-car-generation").textContent=el.generation?el.generation:"No data";
+        el_clone_card.querySelector(".js-car-year").textContent=el.year?el.year:"No data";
+        el_clone_card.querySelector(".js-car-category").textContent=el.category?el.category:"No data";
+        el_clone_card.querySelector(".js-car-link").href=`/details?id=${el.id}`?`/details?id=${el.id}`:`/details?id=${inx}`;
+        el_cards_box.append(el_clone_card);
+    });
+}
+el_pagination_previous.addEventListener("click",()=>{
+    api_skip=api_skip-api_limit;
+    alert(api_skip);
+    mainRequests();
+});
+el_pagination_next.addEventListener("click",()=>{
+    api_skip=api_skip+api_limit;
+    alert(api_skip);
+    mainRequests();
+});
