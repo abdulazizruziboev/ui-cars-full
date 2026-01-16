d0f59c3d254e65a2f0174cd68acc5c5d01251062
abdulaziz <telegrampreimum4@gmail.com>
2026-01-16 09:35:52 +0000

    #test

--- global-theme-js/main_app.js
+++ global-theme-js/main_app.js
@@ -1,18 +1,33 @@
 function SystemModeChanger(mode_type="system") {
     if(localStorage.getItem("theme-mode")==null) {
-        localStorage.setItem("theme-mode",mode_type)
+        localStorage.setItem("theme-mode",mode_type)    
     } else {
         localStorage.setItem("theme-mode",mode_type)
     }
     if(localStorage.getItem("theme-mode")=="system") {
         document.querySelector("html").classList.remove("dark");
         document.querySelector("html").classList.remove("light");
+        /*  */
+        document.querySelector(`.js-theme-btns-system`).classList.add("bg-[#1f8fff]","text-white");
+        document.querySelector(`.js-theme-btns-light`).classList.remove("bg-[#1f8fff]","text-white");
+        document.querySelector(`.js-theme-btns-dark`).classList.remove("bg-[#1f8fff]","text-white");
+        /*  */
     } else if(localStorage.getItem("theme-mode")=="light") {
         document.querySelector("html").classList.remove("dark");
         document.querySelector("html").classList.add("light");
+        /*  */
+        document.querySelector(`.js-theme-btns-light`).classList.add("bg-[#1f8fff]","text-white");
+        document.querySelector(`.js-theme-btns-system`).classList.remove("bg-[#1f8fff]","text-white");
+        document.querySelector(`.js-theme-btns-dark`).classList.remove("bg-[#1f8fff]","text-white");
+        /*  */
     } else if(localStorage.getItem("theme-mode")=="dark") {
         document.querySelector("html").classList.remove("light");
         document.querySelector("html").classList.add("dark");
+        /*  */
+        document.querySelector(`.js-theme-btns-dark`).classList.add("bg-[#1f8fff]","text-white");
+        document.querySelector(`.js-theme-btns-light`).classList.remove("bg-[#1f8fff]","text-white");
+        document.querySelector(`.js-theme-btns-system`).classList.remove("bg-[#1f8fff]","text-white");
+        /*  */
     }
 }
 SystemModeChanger();
